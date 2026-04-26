2de97b2a25a49d409ac275d5475c06b67daa98fd
Eugen Rof <eugenrof@gmail.com>
2026-04-26 12:48:23 +0300

    refactoring images

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/tc-chef-tool/',
+    base: '/tc-chef-tool/', // This tells Vite: "The project lives in this folder"
 });
